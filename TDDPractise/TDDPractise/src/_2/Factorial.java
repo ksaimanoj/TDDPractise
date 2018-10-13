68286f10a71b872b92b4fa16c0788fcea35aeccd
Sai Manoj Kadiyala <saimanoj.kadiyala@gmail.com>
2018-10-13 08:32:03 +0530

    Factorial support for larger numbers.

--- TDDPractise/TDDPractise/src/_2/Factorial.java
+++ TDDPractise/TDDPractise/src/_2/Factorial.java
@@ -1,24 +1,26 @@
 package _2;
 
+import java.math.BigInteger;
+
 public class Factorial {
 	public static class NegativeNumberException extends RuntimeException {
 		private static final long serialVersionUID = 1L;
 	}
 
-	public static int calculate(int value) {
+	public static BigInteger calculate(int value) {
 		validateInput(value);
 		return calculateFactorial(value);
 	}
 
 	private static void validateInput(int value) {
-		if(value < 0)
+		if (value < 0)
 			throw new NegativeNumberException();
 	}
-	
-	private static int calculateFactorial(int value) {
-		int result = 1;
-		for(int i = 1; i <= value; i++)
-			result *= i;
+
+	private static BigInteger calculateFactorial(int value) {
+		BigInteger result = new BigInteger("1");
+		for (int i = 1; i <= value; i++)
+			result = result.multiply(new BigInteger(i+""));
 		return result;
 	}
 }
--- TDDPractise/TDDPractise/src/_2/FactorialTest.java
+++ TDDPractise/TDDPractise/src/_2/FactorialTest.java
@@ -1,5 +1,7 @@
 package _2;
 
+import java.math.BigInteger;
+
 import org.junit.Test;
 
 import junit.framework.Assert;
@@ -12,13 +14,34 @@ public class FactorialTest {
 
 	@Test
 	public void facts() {
-		Assert.assertEquals(1, Factorial.calculate(0));
-		Assert.assertEquals(1, Factorial.calculate(1));
-		Assert.assertEquals(2, Factorial.calculate(2));
+		Assert.assertEquals(new BigInteger("1"), Factorial.calculate(0));
+		Assert.assertEquals(new BigInteger("1"), Factorial.calculate(1));
+		Assert.assertEquals(new BigInteger("2"), Factorial.calculate(2));
 	}
 
 	@Test
 	public void furtherTest() {
-		Assert.assertEquals(120, Factorial.calculate(5));
+		Assert.assertEquals(new BigInteger("120"), Factorial.calculate(5));
+	}
+
+	@Test
+	public void biggerNumbersTest() {
+		Assert.assertEquals(new BigInteger("3628800"), Factorial.calculate(10));
+		Assert.assertEquals(new BigInteger("39916800"),
+				Factorial.calculate(11));
+		Assert.assertEquals(new BigInteger("479001600"),
+				Factorial.calculate(12));
+		intOverflowCases();
+	}
+
+	private void intOverflowCases() {
+		Assert.assertEquals(new BigInteger("6227020800"),
+				Factorial.calculate(13));
+		Assert.assertEquals(new BigInteger("1124000727777607680000"),
+				Factorial.calculate(22));
+		Assert.assertEquals(
+				new BigInteger(
+						"815915283247897734345611269596115894272000000000"),
+				Factorial.calculate(40));
 	}
 }
